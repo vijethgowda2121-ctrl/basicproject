757154ad3178b2c1fdede76c8316eafb7276195f
vijethgowda2121-ctrl <vijethgowda2121@gmail.com>
2026-06-05 13:09:19 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="ML Data Analyzer",
+    page_icon="📊",
+    layout="wide"
+)
+
+st.title("🚀 Machine Learning Data Analyzer")
+
+st.markdown("""
+Upload a dataset and perform:
+
+- EDA
+- Feature Analysis
+- Machine Learning
+- Predictions
+- Model Evaluation
+""")
